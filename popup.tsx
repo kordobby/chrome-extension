53e3c2eee5c6963c5eb9a1e3319a0590090b769a
kordobby <poordobby@naver.com>
2024-04-12 18:00:56 +0900

    add. contenet script - google sidepanel

--- popup.tsx
+++ popup.tsx
@@ -12,7 +12,7 @@ function IndexPopup() {
       <h2>
         Welcome to your
         <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
+          Plasmoasdfasfa
         </a>
         Extension!
       </h2>
